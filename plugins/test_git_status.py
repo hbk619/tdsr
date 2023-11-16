d192dd212e70d2a8230d397157fa98a5985080e6
hbk619 <hbk619@users.noreply.github.com>
2023-11-16 17:36:39 +0000

    Add git status parsing
    Now when you press alt and s it will read out the changes that are staged. We need to reverse the order of the lines to ensure we find the 'staged' line first, then add file names.
    
    Currently if your buffer isn't big enough (aka it scrolled off the top of the screen) it won't work

---
+++ plugins/test_git_status.py
@@ -0,0 +1,25 @@
+from plugins import parse_git_status
+
+status = """On branch my-branchie
+Changes to be committed:
+(use "git restore --staged <file>..." to unstage)
+modified:   ma/path/to/staged.md
+
+Changes not staged for commit:
+(use "git add <file>..." to update what will be committed)
+(use "git restore <file>..." to discard changes in working directory)
+modified:   a_modified_file
+modified:   path/to/a_modified_file
+
+Untracked files:
+(use "git add <file>..." to include in what will be committed)
+.hidden
+some/folder/thing/untracked.md"""
+
+
+def test_git_status_committed():
+    expected = ["modified:   ma/path/to/staged.md"]
+    lines = status.split('\n')
+    lines.reverse()
+    results = parse_git_status.parse_output(lines)
+    assert results == expected
