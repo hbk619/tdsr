42426ddbeaab30bdea72e3d8120e62946cb20255
hbk619 <hbk619@users.noreply.github.com>
2023-11-19 11:41:17 +0000

    Verify no changes message

--- plugins/git_status/test_git_status.py
+++ plugins/git_status/test_git_status.py
@@ -23,3 +23,26 @@ def test_git_status_committed():
     lines.reverse()
     results = parse_output(lines)
     assert results == expected
+
+no_changes = """On branch my-branchie
+Your branch is ahead of 'origin/master' by 2 commits.
+  (use "git push" to publish your local commits)
+
+Changes not staged for commit:
+(use "git add <file>..." to update what will be committed)
+(use "git restore <file>..." to discard changes in working directory)
+modified:   a_modified_file
+modified:   path/to/a_modified_file
+
+Untracked files:
+(use "git add <file>..." to include in what will be committed)
+.hidden
+some/folder/thing/untracked.md"""
+
+
+def test_git_status_committed_no_changes():
+    expected = ["No staged files"]
+    lines = no_changes.split('\n')
+    lines.reverse()
+    results = parse_output(lines)
+    assert results == expected
